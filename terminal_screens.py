74739d01543485eb1d02a03bc601280659a7c487
Ajay Samuel <ajaysamuel16@gmail.com>
2023-03-03 19:39:50 +0800

    Added Blink Feature for Menu Selection

--- terminal_screens.py
+++ terminal_screens.py
@@ -2,6 +2,7 @@
 import time
 from blessed import Terminal
 
+MENU_OPTIONS = [" New Game ", " Load Game ", " Multiplayer ", "Quit" ] # list of options for main menu
 
 term = Terminal()
 
@@ -24,46 +25,58 @@ def title():
     
 def menu_options(option = 0):
     
-    menu_options = [" New Game ", " Load Game ", " Multiplayer ", " Quit " ]
-    
     print("\n")
     print(term.bold_orange(term.center("__________________________________________________________________________________________")))
     print("\n")
     
     if option == 0:
-        print(term.center(term.bold_orange_reverse(menu_options[0])))
+        print(term.center(term.bold_orange_reverse(MENU_OPTIONS[0])))
+        position = 5 #
     else:
-        print(term.center(term.bold_orange(menu_options[0])))
+        print(term.center(term.bold_orange(MENU_OPTIONS[0])))
         
     if option == 1:
-        print(term.center(term.bold_orange_reverse(menu_options[1])))
+        print(term.center(term.bold_orange_reverse(MENU_OPTIONS[1])))
+        position = 6 #
     else:
-        print(term.center(term.bold_orange(menu_options[1])))
+        print(term.center(term.bold_orange(MENU_OPTIONS[1])))
 
     if option == 2:
-        print(term.center(term.bold_orange_reverse(menu_options[2])))
+        print(term.center(term.bold_orange_reverse(MENU_OPTIONS[2])))
+        position = 7 #
     else:
-        print(term.center(term.bold_orange(menu_options[2])))
+        print(term.center(term.bold_orange(MENU_OPTIONS[2])))
     
     if option == 3:
-        print(term.center(term.bold_orange_reverse(menu_options[3])))
+        print(term.center(term.bold_orange_reverse(MENU_OPTIONS[3])))
+        position = 8 #
     else:
-        print(term.center(term.bold_orange(menu_options[3])))
+        print(term.center(term.bold_orange(MENU_OPTIONS[3])))
 
-    
     print(term.bold_orange(term.center("__________________________________________________________________________________________")))
-    print("\n")
+    
+    return position # returns cursor position of the option in the menu
+    
+def option_blink(position):
+    
+    print(term.move_up(position), end='')
+    print(term.center(term.bold_orange(MENU_OPTIONS[0])))
+    print(term.move_down(position), end='')
 
+    time.sleep(0.6)
+
+    print(term.move_up(position), end='')
+    print(term.center(term.bold_orange_reverse(MENU_OPTIONS[0])))
+    print(term.move_down(position), end='')
+    
+    
 def menu_main():
     
-    title()
-    menu_options()
-    #while True:
-    print(term.move_up(7) + "")
-    
-    print(term.move_down(7))
-    
-
-        
+        title()
+        cur_position = menu_options()
+        while term.inkey(timeout=0.6) != 'q':
+            option_blink(cur_position)
+        print(term.clear())
+            
     #time.sleep(0.8)
     #print(term.clear())
