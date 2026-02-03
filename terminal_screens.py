8a066845e2301750f2247ad70639e5496d656baa
Ajay Samuel <ajaysamuel16@gmail.com>
2026-02-04 02:35:15 +0800

    adding blink function

--- terminal_screens.py
+++ terminal_screens.py
@@ -9,15 +9,15 @@ term = Terminal()
 def title():
     
     print(term.bold_cyan(term.center("__________________________________________________________________________________________")))
-    print(term.bold_cyan(term.center("  _______ __                  _______                          ________                   ")))
-    print(term.bold_cyan(term.center(" |       \  \                |       \                        |        \                  ")))
-    print(term.bold_cyan(term.center("   \▓▓▓▓▓▓▓▓\▓▓ _______        \▓▓▓▓▓▓▓▓ ______   _______        \▓▓▓▓▓▓▓▓ ______   ______   ")))
+    print(term.bold_cyan(term.center("  ______  __                  _______                           _______                    ")))
+    print(term.bold_cyan(term.center(" |      \ \ \                |       \                         |       \                  ")))
+    print(term.bold_cyan(term.center("  \▓▓▓▓▓▓▓▓ ▓▓ _______        \▓▓▓▓▓▓▓▓ ______   _______        \▓▓▓▓▓▓▓▓ ______   ______   ")))
     print(term.bold_cyan(term.center("    | ▓▓  |  \/       \______   | ▓▓   |      \ /       \______   | ▓▓   /      \ /      \  ")))
     print(term.bold_cyan(term.center("   | ▓▓  | ▓▓  ▓▓▓▓▓▓▓      \  | ▓▓    \▓▓▓▓▓▓\  ▓▓▓▓▓▓▓      \  | ▓▓  |  ▓▓▓▓▓▓\  ▓▓▓▓▓▓\\")))
     print(term.bold_cyan(term.center("    | ▓▓  | ▓▓ ▓▓      \▓▓▓▓▓▓  | ▓▓   /      ▓▓ ▓▓      \▓▓▓▓▓▓  | ▓▓  | ▓▓  | ▓▓ ▓▓    ▓▓ ")))
     print(term.bold_cyan(term.center("    | ▓▓  | ▓▓ ▓▓_____          | ▓▓  |  ▓▓▓▓▓▓▓ ▓▓_____          | ▓▓  | ▓▓__/ ▓▓ ▓▓▓▓▓▓▓▓ ")))
     print(term.bold_cyan(term.center("   | ▓▓  | ▓▓\▓▓     \         | ▓▓   \▓▓    ▓▓\▓▓     \         | ▓▓   \▓▓    ▓▓\▓▓     \\")))
-    print(term.bold_cyan(term.center("                     \▓▓   \▓▓ \▓▓▓▓▓▓▓          \▓▓    \▓▓▓▓▓▓▓ \▓▓▓▓▓▓▓          \▓▓    \▓▓▓▓▓▓  \▓▓▓▓▓▓▓  by BlessCoffee ")))  
+    print(term.bold_cyan(term.center("                    \▓▓   \▓▓ \▓▓▓▓▓▓▓          \▓▓    \▓▓▓▓▓▓▓ \▓▓▓▓▓▓▓          \▓▓    \▓▓▓▓▓▓  \▓▓▓▓▓▓▓  by Ajay Samuel")))  
     print(term.bold_cyan(term.center(" __________________________________________________________________________________________")))
     
     # Art by ASCII Generator by https://texteditor.com/multiline-text-art/
@@ -30,50 +30,42 @@ def menu_options(option = 0):
     
     if option == 0:
         print(term.center(term.bold_orange_reverse(MENU_OPTIONS[0])))
-        position = 5 #
     else:
         print(term.center(term.bold_orange(MENU_OPTIONS[0])))
         
     if option == 1:
         print(term.center(term.bold_orange_reverse(MENU_OPTIONS[1])))
-        position = 6 #
     else:
         print(term.center(term.bold_orange(MENU_OPTIONS[1])))
 
     if option == 2:
         print(term.center(term.bold_orange_reverse(MENU_OPTIONS[2])))
-        position = 7 #
     else:
         print(term.center(term.bold_orange(MENU_OPTIONS[2])))
     
     if option == 3:
         print(term.center(term.bold_orange_reverse(MENU_OPTIONS[3])))
-        position = 8 #
     else:
         print(term.center(term.bold_orange(MENU_OPTIONS[3])))
 
     print(term.bold_orange(term.center("__________________________________________________________________________________________")))
     
-    return position # returns cursor position of the option in the menu
     
-def option_blink(position):
-    #bugged needs fixing
-    print(term.move_up(position), end='')
-    print(term.center(term.bold_orange(MENU_OPTIONS[0])))
-    print(term.move_down(position), end='')
-
-    time.sleep(0.6)
-
-    print(term.move_up(position), end='')
-    print(term.center(term.bold_orange_reverse(MENU_OPTIONS[0])))
-    print(term.move_down(position), end='')
+def option_blink(option):
+    term.move_xy(0,0)
+    option_postion_x = option + 17
+    print(term.home + term.center(term.move_down(option_postion_x) + term.bold_orange(MENU_OPTIONS[option])))
+    time.sleep(0.7)
+    print(term.home + term.center(term.move_down(option_postion_x) + term.bold_orange_reverse(MENU_OPTIONS[option])))
 
 def menu_main():
-    with term.cbreak(), term.hidden_cursor():
+    with term.cbreak():
+        option = 0
         print(term.home + term.clear_eos)
         title()
-        option = menu_options()
+        menu_options(option)
         while True:
+            option_blink(option)
             key = term.inkey(timeout=0.6)
             if key.name == "KEY_UP":
                 option = (option - 1) % len(MENU_OPTIONS)
@@ -89,7 +81,7 @@ def menu_main():
                 # if option == MENU_OPTIONS.index(" Quit "):
                 # os.system('cls' if os.name == 'nt' else 'clear')
                 # return
-                return option
+                return os.system('cls' if os.name == 'nt' else 'clear')
             elif key == 'q':
                 os.system('cls' if os.name == 'nt' else 'clear')
                 return len(MENU_OPTIONS) - 1  # Return the index for "Quit" option
